50fc4ce3ecbc4b43d6dd9e3553ae3aab8de11de6
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-09 02:28:33 -0800

    six commit

--- index.js
+++ index.js
@@ -47,12 +47,24 @@ async function run(){
         });
 
         // reviews api
-
+        app.get('/reviews',async(req,res)=>{
+            
+            let query = {};
+            if(req.query.email){
+                query ={
+                    email:req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
+        })
         app.post('/reviews',async(req,res)=>{
             const order = req.body;
             const result = await reviewCollection.insertOne(order);
             res.send(result)
-        })
+        });
+
     }
     finally{
 
